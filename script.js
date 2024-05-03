2add979348fb57cae9322b237d15eb98992bd7dc
Shashank sahu <144484964+Shashank84-creator@users.noreply.github.com>
2024-05-03 18:54:13 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,8 @@
+() => { 
+  cy.visit(baseUrl + "/main.html"); 
+	 cy.get("#button-5").should("have.class", "btn btn-warning btn-lg btn-full-width");
+  cy.get("#button-6")
+    .should("have.class", "btn")
+    .and("have.class", "btn-info")
+    .and("have.attr", "disabled");
+}
