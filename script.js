37ebe0deb65d3a0f198b1f43a2faec9a66809251
Shashank sahu <144484964+Shashank84-creator@users.noreply.github.com>
2024-05-03 18:35:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-//your code here
-cy.visit(baseUrl + "/main.html");
-cy.get("#button-6").should("be.disabled");
+() => { 
+  cy.visit(baseUrl + "/main.html"); 
+  cy.get("#button-6").should("have.class", "btn btn-info"); 
+  cy.get("#button-6").should("be.disabled"); 
+  cy.get("#button-5").should("have.class", "btn btn-warning"); 
+  cy.get("#button-5").should("have.class", "btn-lg"); 
+  cy.get("#button-5").should("have.css", "width", "100%"); 
+}
